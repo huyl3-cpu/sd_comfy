0648173533c1231768de52e5282b9a98d7d478c6
huyl3 <59508262+huyl3-cpu@users.noreply.github.com>
2026-01-22 22:42:59 +0700

    Update m_wan22.py

--- m_wan22.py
+++ m_wan22.py
@@ -4,7 +4,6 @@ def run(cmd: str):
     print(f"\n RUN: {cmd}")
     subprocess.run(cmd, shell=True, check=True)
 
-run("bash -c 'source /content/env.txt || true'")
 run(
     'aria2c -x 16 -s 16 -k 1M "https://huggingface.co/Wan-AI/Wan2.2-Animate-14B/resolve/main/process_checkpoint/det/yolov10m.onnx" '
     '-o "yolov10m.onnx" '
@@ -70,3 +69,4 @@ run(
 )
 
 print("\n✅: Tất cả model đã được tải xuống xong")
+
